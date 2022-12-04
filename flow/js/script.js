3dc469d24a2a675dd1ae86c6cd14f344234a2b57
nexusptx <107808818+nexusptx@users.noreply.github.com>
2022-12-04 01:29:25 -0300

    Delete flow/js directory

--- flow/js/script.js
+++
@@ -1,7 +0,0 @@
-function goToCounter() {
-    window.open('../html/counter.html')
-        }
-
-        function goToGradient() {
-            window.open('../html/generateGradients.html')
-                }
