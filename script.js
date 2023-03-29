fd4deaa024b252ed97fb4b87c9b4da90257d35ea
NexiMi <107808818+nexusptx@users.noreply.github.com>
2023-03-29 15:13:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const btng = document.querySelector('#btng')
+const btns = document.querySelector('#btns')
+const btnd = document.querySelector('#btnd')
+
+
+btng.addEventListener('click', function() {
+    window.open("https://github.com/nexusptx")
+  });
+
+  btnd.addEventListener('click', function() {
+    window.open("https://discord.com/users/714821119080923177")
+  });
+
+  btns.addEventListener('click', function() {
+    window.open("https://open.spotify.com/user/31nzsjluoe7w6irazippn5vcrbye")
+  })
